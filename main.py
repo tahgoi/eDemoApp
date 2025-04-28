4e420945c706986d2ba1e3a03041661d6490bee7
Joe Neil <jstaguan@gmail.com>
2025-04-28 11:22:34 -0400

    Dockerized webapp updated

--- main.py
+++
@@ -1,4 +0,0 @@
-from diffusers import StableDiffusionPipeline
-
-pipeline = StableDiffusionPipeline.from_pretrained("runwayml/stable-diffusion-v1-5")
-pipeline.save_pretrained("./stable-diffusion-model")
